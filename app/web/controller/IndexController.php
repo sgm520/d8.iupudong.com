5180b0045d7aef30e6f8c7507ec529994eb1a64c
e <982018414@qq.com>
2021-07-20 15:59:03 +0800

    'dd'

--- app/web/controller/IndexController.php
+++ app/web/controller/IndexController.php
@@ -32,75 +32,71 @@ class IndexController extends HomeBaseController
 
     public function block()
     {
-        // foreach($data_ChaoshiCategory as $k=>$v){
-        //     $data_ChaoshiCategory[$v['id']] = $v;
-        // }
-
-        $data_ChaoshiCategory = ChaoshiCategoryModel::where('state', 1)->order("list_order", "asc")->select()->toArray();
-
-        foreach ($data_ChaoshiCategory as $k => $v) {
-            $data_ChaoshiCategory[$k]['children'] = ChaoshiModel::where('status', 1)->where('category', $v['id'])->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->order("list_order", "asc")->select()->toArray();
-            // $data_ChaoshiCategory[$k]['children']['update_time_text'] = date('Y-m-d H:i:s',$v['children']['update_time']);
-        }
-
-        echo json_encode(["code" => 200, "data" => $data_ChaoshiCategory], JSON_UNESCAPED_UNICODE);
-        die;
-
-
         $name = $this->request->param("name");
-        // $data_ChaoshiCategory=ChaoshiCategoryModel::where([])->field('id,name,logo')->select()->toArray();
-
-        if (!empty($name)) {
-            $data_Chaoshi = ChaoshiModel::where('status', 1)->where('name', 'like', "{$name}%")->order("list_order", "asc")->select()->toArray();
-
-            foreach ($data_Chaoshi as $k => $v) {
-                $catgory                              = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
-                $data_Chaoshi[$k]['cate']             = $catgory['cate'];
-                $data_Chaoshi[$k]['cate_name']        = $catgory['cate_name'];
-                $data_Chaoshi[$k]['cate_logo']        = $catgory['cate_logo'];
-                $data_Chaoshi[$k]['update_time_text'] = date('Y-m-d H:i:s', $v['update_time']);
-            }
-
-            echo json_encode(["code" => 200, "data" => $data_Chaoshi], JSON_UNESCAPED_UNICODE);
-            die;
-
-        } else {
-            $data_Chaoshi = ChaoshiModel::where('status', 1)->select()->toArray();
-
-            foreach ($data_Chaoshi as $k => $v) {
-                $catgory                              = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
-                $data_Chaoshi[$k]['cate']             = $catgory['cate'];
-                $data_Chaoshi[$k]['cate_name']        = $catgory['cate_name'];
-                $data_Chaoshi[$k]['cate_logo']        = $catgory['cate_logo'];
-                $data_Chaoshi[$k]['update_time_text'] = date('Y-m-d H:i:s', $v['update_time']);
-            }
-
-            echo json_encode(["code" => 200, "data" => $data_Chaoshi], JSON_UNESCAPED_UNICODE);
+        if (empty($name)) {
+            echo json_encode(["code" => 0, "msg" => "name不能为空"], JSON_UNESCAPED_UNICODE);
             die;
         }
-
-
-        if (!empty($name)) {
-            $chaoshi = ChaoshiModel::field('ch.*,ca.name as category_name,ca.category as cate,ca.logo as cate_logo')
-                ->alias('ch')->join('chaoshi_category ca', 'ca.id = ch.category')
-                ->where("ch.status", 1)
-                ->where('ch.name', 'like', "{$name}%")
-                ->select();
-
-        } else {
-            $chaoshi = ChaoshiModel::field('ch.*,ca.name as category_name,ca.category as cate,ca.logo as cate_logo')
-                ->alias('ch')->join('chaoshi_category ca', 'ca.id = ch.category')
-                ->where("ch.status", 1)
-                ->select();
+        if ($name == 'today_hot') {
+            $hot_list  = ChaoshiModel::where('status', 1)->where('hot_list', 1)->order("list_order", "asc")->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->select()->toArray();//热门排行
+            $today_hot = ChaoshiModel::where('status', 1)->where('today_hot', 1)->order("list_order", "asc")->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->select()->toArray(); //今日热门
+            foreach ($hot_list as $k => $v) {
+                $catgory                   = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
+                $hot_list[$k]['cate']      = $catgory['cate'];
+                $hot_list[$k]['cate_name'] = $catgory['cate_name'];
+                $hot_list[$k]['cate_logo'] = $catgory['cate_logo'];
+            }
+            foreach ($today_hot as $k => $v) {
+                $catgory                    = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
+                $today_hot[$k]['cate']      = $catgory['cate'];
+                $today_hot[$k]['cate_name'] = $catgory['cate_name'];
+                $today_hot[$k]['cate_logo'] = $catgory['cate_logo'];
+            }
+            echo json_encode(["code" => 200, "data" => ['hot' => $hot_list, 'today' => $today_hot]], JSON_UNESCAPED_UNICODE);
+            die;
+        } elseif ($name == 'today_update') {
+            $data = ChaoshiModel::where('status', 1)->where('today_update', 1)->order("list_order", "asc")->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->select()->toArray();//今日更新
+            foreach ($data as $k => $v) {
+                $catgory               = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
+                $data[$k]['cate']      = $catgory['cate'];
+                $data[$k]['cate_name'] = $catgory['cate_name'];
+                $data[$k]['cate_logo'] = $catgory['cate_logo'];
+            }
+            echo json_encode(["code" => 200, "data" => $data], JSON_UNESCAPED_UNICODE);
+            die;
+        } elseif ($name == "new_loan") {
+            $data = ChaoshiModel::where('status', 1)->where('new_loan', 1)->order("list_order", "asc")->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->select()->toArray();//今日更新
+            foreach ($data as $k => $v) {
+                $catgory               = ChaoshiCategoryModel::where(['id' => $v['category']])->field('id cate,name cate_name,logo cate_logo')->find()->toArray();
+                $data[$k]['cate']      = $catgory['cate'];
+                $data[$k]['cate_name'] = $catgory['cate_name'];
+                $data[$k]['cate_logo'] = $catgory['cate_logo'];
+            }
+            echo json_encode(["code" => 200, "data" => $data], JSON_UNESCAPED_UNICODE);
+            die;
+        } elseif ($name == 'series') {
+            $data_ChaoshiCategory = ChaoshiCategoryModel::where('state', 1)->where('category', 1)->order("list_order", "asc")->select()->toArray();
+            foreach ($data_ChaoshiCategory as $k => $v) {
+                $data_Chaoshi = ChaoshiModel::where('status', 1)->where('category', $v['id'])->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->order("list_order", "asc")->select()->toArray();
+                foreach ($data_Chaoshi as $k1 => $v2) {
+                    $data_Chaoshi[$k1]['cate']      = $v['id'];
+                    $data_Chaoshi[$k1]['cate_name'] = $v['name'];
+                    $data_Chaoshi[$k1]['cate_logo'] = $v['logo'];
+                }
+                $data_ChaoshiCategory[$k]['children'] = $data_Chaoshi;
+            }
+            echo json_encode(["code" => 200, "data" => $data_ChaoshiCategory], JSON_UNESCAPED_UNICODE);
+            die;
+        } elseif ($name == "lending") {
+            $data_ChaoshiCategory = ChaoshiCategoryModel::where('state', 1)->where('category', 2)->order("list_order", "asc")->select()->toArray();
+            foreach ($data_ChaoshiCategory as $k => $v) {
+                $data_ChaoshiCategory[$k]['children'] = ChaoshiModel::where('status', 1)->where('category', $v['id'])->field('*,FROM_UNIXTIME(update_time,"%Y-%m-%d %H:%i:%s") update_time_text')->order("list_order", "asc")->select()->toArray();
+            }
+            echo json_encode(["code" => 200, "data" => $data_ChaoshiCategory], JSON_UNESCAPED_UNICODE);
+            die;
         }
 
-
-        foreach ($chaoshi as $k => $v) {
-            $chaoshi[$k]['update_time_text'] = date('Y-m-d H:i:s', $v['update_time']);
-        }
-
-
-        echo json_encode(["code" => 200, "data" => $chaoshi], JSON_UNESCAPED_UNICODE);
+        echo json_encode(["code" => 0, "msg" => "非法数据"], JSON_UNESCAPED_UNICODE);
         die;
     }
 
